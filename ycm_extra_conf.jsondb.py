22e54a60098ab7f1d0bf5b0614b7e11bb07b9ec7
Peter Szabados <kangirigungi@gmail.com>
2015-07-02 21:56:01 +0200

    Add possibility to set compile_commands.json path in the config file

--- ycm_extra_conf.jsondb.py
+++ ycm_extra_conf.jsondb.py
@@ -6,10 +6,12 @@ def DirectoryOfThisScript():
 import sys
 sys.path.insert(0, DirectoryOfThisScript())
 
-# Set this to the absolute path to the folder (NOT the file!) containing the
-# compile_commands.json file to use that instead of 'flags'. See here for
-# more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
-compilation_database_folder = DirectoryOfThisScript()
+import ycm_jsondb_config
+if "GetCompilationDatabaseFolder" in dir(ycm_jsondb_config):
+  compilation_database_folder = ycm_jsondb_config.GetCompilationDatabaseFolder(
+      DirectoryOfThisScript())
+else:
+  compilation_database_folder = DirectoryOfThisScript()
 
 import ycm_jsondb_core
 ycm_jsondb_core.Init(compilation_database_folder)
